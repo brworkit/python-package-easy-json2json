57c14dc9d55fb11dccf58a9f6cff7c959282f231
brworkit <brworkit@gmail.com>
2020-06-04 20:02:53 -0300

    pep8

--- easyjson2json/json2json.py
+++ easyjson2json/json2json.py
@@ -6,40 +6,40 @@ class Json2Json(object):
         self.__template = template
         self.__source = source
         self.__translated = None
-        self.translate()
+        self._translate()
 
     def get_result(self):
         return self.__translated
 
-    def translate(self):
-        self.__translated = self.analyse(self.__template, self.__source)
+    def _translate(self):
+        self.__translated = self._analyse(self.__template, self.__source)
         return self.__translated
 
-    def get_field_name(self, field, template):        
+    def _get_source_field_name(self, field, template):        
         if "_source" in template[field]:            
             return template[field].pop("_source")
         elif is_type(template[field], dict, list):
             return field                                        
         return template[field]
     
-    def read_list(self, template_field_value, source_field_value):        
-        return [self.analyse(template_field_value, source_from_array) for source_from_array in source_field_value]
+    def _read_list(self, template_field_value, source_field_value):        
+        return [self._analyse(template_field_value, source_from_array) for source_from_array in source_field_value]
 
-    def template_is_dict(self, field, template, source):
-        source_field_name = self.get_field_name(field, template)                
+    def _template_is_dict(self, field, template, source):
+        source_field_name = self._get_source_field_name(field, template)                
         if is_type(source[source_field_name], str, int, float):
             return get_value(source_field_name, source)
         if is_type(source[source_field_name], list):
-            return self.read_list(template[field], source[source_field_name])                                                                
-        return self.analyse(template[field], source[source_field_name])
+            return self._read_list(template[field], source[source_field_name])                                                                
+        return self._analyse(template[field], source[source_field_name])
 
-    def template_is_list(self, field, template, source):        
+    def _template_is_list(self, field, template, source):        
         return query_field_value(template[field] , source)
 
-    def template_is_str(self, field, template, source):                
-        return get_value(self.get_field_name(field, template), source)
+    def _template_is_str(self, field, template, source):                
+        return get_value(self._get_source_field_name(field, template), source)
 
-    def analyse(self, template, source):                
+    def _analyse(self, template, source):                
         result = {}
         for field in template:            
             if template[field] is None:
@@ -47,11 +47,11 @@ class Json2Json(object):
             else:                
                 template_field_value = template[field]                
                 if is_type(template_field_value, dict):
-                    result[field] = self.template_is_dict(field, template, source)
+                    result[field] = self._template_is_dict(field, template, source)
                 elif is_type(template_field_value, list):  
-                    result[field] = self.template_is_list(field, template, source)
+                    result[field] = self._template_is_list(field, template, source)
                 elif is_type(template_field_value, str):
-                    result[field] = self.template_is_str(field, template, source)                
+                    result[field] = self._template_is_str(field, template, source)                
         return result
 
      
